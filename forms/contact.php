3304a74b46911698c9365e2c08e12a6361cbc9d4
Jacob Wehrle <chas@radcraftsman.ca>
2023-03-05 13:01:47 -0500

    fix links and first tyr at contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,63 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+ 
+if($_POST) {
+    $name = "";
+    $email = "";
+    $email_subject = "Website Inquiry: General Contact Form!";
+    $subject = "";
+    $message = "";
+    $recipient = "chaswehrlzz@gmail.com";
+    $data = [];
+     
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
+    }
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+     
+    if(isset($_POST['subject'])) {
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Subject of message:</b></label>&nbsp;<span>".$subject."</span>
+                        </div>";
+    }
+     
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+     
+    $email_body .= "</div>";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+     
+    if(mail($recipient, $email_subject, $email_body, $headers)) {
+        $data['success'] = true;
+        $data['message'] = "Thank you for contacting us, $name. You will get a reply within 24 hours";
+        echo json_encode($data);
+    } else {
+        $data['success'] = false;
+        $data['message'] = "We are sorry but the email did not go through, please try again";
+        echo json_encode($data);
+    }
+     
+} else {
+    $data['success'] = false;
+    $data['message'] = "Something went wrong";
+    echo json_encode($data);
+}
 ?>
