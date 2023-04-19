a6f7ea4a702838b04c4dc13483322edaa84e3bd3
Jacob Wehrle <chas@radcraftsman.ca>
2023-04-18 22:29:15 -0400

    test static email

--- forms/form.js
+++ forms/form.js
@@ -1,44 +1,66 @@
-$(document).ready(function () {
-    $("form").submit(function (event) {
-        $(".form-group").removeClass("has-error");
-        $(".help-block").remove();
-
-        var formData = {
-            name: $("#name").val(),
-            email: $("#email").val(),
-            subject: $("#subject").val(),
-            message: $("#message").val(),
-        };
-  
+$(document).ready(function() {
+    $('.php-email-form').submit(function(e) {
+        e.preventDefault();
+        var form = $(this);
+        var formData = form.serialize();
         $.ajax({
-            type: "POST",
-            url: "https://downnorthgarlic.com/forms/contact.php",
+            type: 'POST',
+            url: 'forms/contact.php',
             data: formData,
-            dataType: "json",
-            encode: true,
-            success: function(data) { console.log("success") },
-            error: function(ts) { console.log("failed") }
-        }).done(function (data) {
-            console.log(data);
-
-            if (!data.success) {
-                console.log(data);
-                $("form").html(
-                    '<div class="alert alert-danger">Form inputs were wrong, please try again.</div>'
-                );
-            } else {
-                $("form").html(
-                    '<div class="alert alert-success">' + data.message + "</div>"
-                );
+            success: function(response) {
+                alert('Your message was sent!');
+                form.trigger('reset');
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+                alert('There was an error sending your message. Please try again later.');
             }
-        })
-        .fail(function (data) {
-            console.log(data);
-            $("form").html(
-                '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
-            );
         });
-
-        event.preventDefault();
     });
 });
+// $(document).ready(function () {
+//     $("form").submit(function (event) {
+//         $(".form-group").removeClass("has-error");
+//         $(".help-block").remove();
+
+//         var formData = {
+//             name: $("#name").val(),
+//             email: $("#email").val(),
+//             subject: $("#subject").val(),
+//             message: $("#message").val(),
+//         };
+  
+//         $.ajax({
+//             type: "POST",
+//             url: "https://downnorthgarlic.com/forms/contact.php",
+//             data: formData,
+//             dataType: "json",
+//             encode: true,
+//             success: function(data) { console.log("success") },
+//             error: function(ts) { console.log("failed") }
+//         }).done(function (data) {
+//             console.log(data);
+
+//             if (!data.success) {
+//                 console.log(data);
+//                 $("form").html(
+//                     '<div class="alert alert-danger">Form inputs were wrong, please try again.</div>'
+//                 );
+//             } else {
+//                 $("form").html(
+//                     '<div class="alert alert-success">' + data.message + "</div>"
+//                 );
+//             }
+//         })
+//         .fail(function (data) {
+//             console.log("form data sent: ")
+//             console.log(formData)
+//             console.log(data);
+//             $("form").html(
+//                 '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
+//             );
+//         });
+
+//         event.preventDefault();
+//     });
+// });
