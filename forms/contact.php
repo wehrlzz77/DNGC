2b0d4a996e4cdf10fc3d0d11789fc609e499dd5b
Jacob Wehrle <chas@radcraftsman.ca>
2023-04-18 22:46:26 -0400

    test static email

--- forms/contact.php
+++ forms/contact.php
@@ -3,151 +3,99 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require __DIR__ . '/vendor/autoload.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_POST) {
+    // $name = "";
+    // $email = "";
+    // $email_subject = "Website Inquiry: General Contact Form!";
+    // $subject = "";
+    // $message = "";
+    // // $recipient = "andrew@downnorthgarlic.com";
+    // $recipient = "andrew.russell19@gmail.com";
+    // $data = [];
 
-    // Get the form fields and sanitize the input
-    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    $mail = new PHPMailer();
 
-    // Initialize the PHPMailer object
-    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'andrew@downnorthgarlic.com';
+    $mail->Password = 'Andyrussell1';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 25;
 
-    try {
-        // Set up SMTP
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'wehrlzz77@gmail.com'; // Replace with your own email address
-        $mail->Password = 'Lucille77'; // Replace with your own password
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
+    $mail->setFrom('contact@downnorthgarlic.com', 'Web Contact Form');
+    $mail->addReplyTo('contact@downnorthgarlic.com', 'Web Contact Form');
 
-        // Set up message data
-        $mail->setFrom($email, $name);
-        $mail->addAddress('recipient@example.com', 'Recipient Name');
-        $mail->addReplyTo($email, $name);
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body = $message;
+    $mail->addAddress('andrew@downnorthgarlic.com');
+    $mail->Subject = 'Website Inquiry: General Contact Form!';
+    $mail->isHTML(true);
 
-        // Send the email
-        $mail->send();
+    $email_body = "<div>";
 
-        echo "success";
-    } catch (Exception $e) {
-        echo $e->getMessage();
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
     }
+
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+     
+    if(isset($_POST['subject'])) {
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Subject of message:</b></label>&nbsp;<span>".$subject."</span>
+                        </div>";
+    }
+     
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+     
+    $email_body .= "</div>";
+
+    $mail->Body = $email_body;
+
+    $mail->SMTPDebug = 2;
+
+    // $headers  = 'MIME-Version: 1.0' . "\r\n"
+    // .'Content-type: text/html; charset=utf-8' . "\r\n"
+    // .'From: ' . $email . "\r\n";
+    echo ("What the fuck");
+    if(!$mail->send()) {
+        $data['success'] = false;
+        // $data['message'] = "We are sorry but the email did not go through, please try again";
+        $data['message'] = 'Mailer Error: ' . $mail->ErrorInfo;
+        echo json_encode($data);
+    } else {
+        $data['success'] = true;
+        $data['message'] = "Thank you for contacting us, $name. You will get a reply within 24 hours";
+        echo json_encode($data);
+    }
+
+    // if(mail($recipient, $email_subject, $email_body, $headers)) {
+    //     $data['success'] = true;
+    //     $data['message'] = "Thank you for contacting us, $name. You will get a reply within 24 hours";
+    //     echo json_encode($data);
+    // } else {
+    //     $data['success'] = false;
+    //     $data['message'] = "We are sorry but the email did not go through, please try again";
+    //     echo json_encode($data);
+    // }
+     
+} else {
+    $data['success'] = false;
+    $data['message'] = "Something went wrong";
+    echo json_encode($data);
 }
-
 ?>
-
-// if($_POST) {
-//     // $name = "";
-//     // $email = "";
-//     // $email_subject = "Website Inquiry: General Contact Form!";
-//     // $subject = "";
-//     // $message = "";
-//     // // $recipient = "andrew@downnorthgarlic.com";
-//     // $recipient = "andrew.russell19@gmail.com";
-//     // $data = [];
-
-//     try {
-//         $mail = new PHPMailer();
-
-//         $mail->isSMTP();
-//         $mail->SMTPDebug = 4;
-//         $mail->Debugoutput = 'html';
-//         $mail->Host = 'smtp.gmail.com';
-//         $mail->SMTPAuth = true;
-//         $mail->Username = 'andrew@downnortdhgarlic.com';
-//         $mail->Password = 'Andyrussell1';
-//         $mail->SMTPSecure = 'tls';
-//         $mail->Port = 25;
-
-//         $mail->setFrom('contact@downnorthgarlic.com', 'Web Contact Form');
-//         $mail->addReplyTo('contact@downnorthgarlic.com', 'Web Contact Form');
-
-//         $mail->addAddress('andrew@downnorthgarlic.com');
-//         $mail->Subject = 'Website Inquiry: General Contact Form!';
-//         $mail->isHTML(true);
-//         $mail->Body = 'Test test 123.'
-
-//         $mail->send();
-//         echo 'Message has been sent';
-//     } catch (\Throwable $th) {
-//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-//     }
-    
-
-//     // $email_body = "<div>";
-
-//     // if(isset($_POST['name'])) {
-//     //     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-//     //     $email_body .= "<div>
-//     //                        <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
-//     //                     </div>";
-//     // }
-
-//     // if(isset($_POST['email'])) {
-//     //     $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-//     //     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-//     //     $email_body .= "<div>
-//     //                        <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
-//     //                     </div>";
-//     // }
-     
-//     // if(isset($_POST['subject'])) {
-//     //     $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-//     //     $email_body .= "<div>
-//     //                        <label><b>Subject of message:</b></label>&nbsp;<span>".$subject."</span>
-//     //                     </div>";
-//     // }
-     
-//     // if(isset($_POST['message'])) {
-//     //     $message = htmlspecialchars($_POST['message']);
-//     //     $email_body .= "<div>
-//     //                        <label><b>Visitor Message:</b></label>
-//     //                        <div>".$message."</div>
-//     //                     </div>";
-//     // }
-     
-//     // $email_body .= "</div>";
-
-//     // $mail->Body = $email_body;
-
-//     // $mail->SMTPDebug = 3;
-
-//     // // $headers  = 'MIME-Version: 1.0' . "\r\n"
-//     // // .'Content-type: text/html; charset=utf-8' . "\r\n"
-//     // // .'From: ' . $email . "\r\n";
-//     // echo ("What the fuck");
-//     // if(!$mail->send()) {
-//     //     $data['success'] = false;
-//     //     // $data['message'] = "We are sorry but the email did not go through, please try again";
-//     //     $data['message'] = 'Mailer Error: ' . $mail->ErrorInfo;
-//     //     echo json_encode($data);
-//     // } else {
-//     //     $data['success'] = true;
-//     //     $data['message'] = "Thank you for contacting us, $name. You will get a reply within 24 hours";
-//     //     echo json_encode($data);
-//     // }
-
-
-//     // if(mail($recipient, $email_subject, $email_body, $headers)) {
-//     //     $data['success'] = true;
-//     //     $data['message'] = "Thank you for contacting us, $name. You will get a reply within 24 hours";
-//     //     echo json_encode($data);
-//     // } else {
-//     //     $data['success'] = false;
-//     //     $data['message'] = "We are sorry but the email did not go through, please try again";
-//     //     echo json_encode($data);
-//     // }
-     
-// } else {
-//     $data['success'] = false;
-//     $data['message'] = "Something went wrong";
-//     echo json_encode($data);
-// }
-// ?>
--- forms/form.js
+++ forms/form.js
@@ -1,74 +1,44 @@
-$(document).ready(function() {
-    $('.php-email-form').submit(function(e) {
-        e.preventDefault();
-        // Get the form data
-        var formData = $(this).serialize();
-        console.log(formData);
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        $(".form-group").removeClass("has-error");
+        $(".help-block").remove();
 
-        // Send an AJAX request to the contact.php file
+        var formData = {
+            name: $("#name").val(),
+            email: $("#email").val(),
+            subject: $("#subject").val(),
+            message: $("#message").val(),
+        };
+  
         $.ajax({
             type: "POST",
-            url: "contact.php",
+            url: "https://downnorthgarlic.com/forms/contact.php",
             data: formData,
-            success: function(response) {
-                if (response == "success") {
-                // Do something on success
-                console.log("Email sent successfully.");
-                } else {
-                // Log the error message to the console
-                console.log(response);
-                }
-            },
-            error: function(xhr, status, error) {
-                // Log the error message to the console
-                console.log(error);
+            dataType: "json",
+            encode: true,
+            success: function(data) { console.log("success") },
+            error: function(ts) { console.log("failed") }
+        }).done(function (data) {
+            console.log(data);
+
+            if (!data.success) {
+                console.log(data);
+                $("form").html(
+                    '<div class="alert alert-danger">Form inputs were wrong, please try again.</div>'
+                );
+            } else {
+                $("form").html(
+                    '<div class="alert alert-success">' + data.message + "</div>"
+                );
             }
+        })
+        .fail(function (data) {
+            console.log(data);
+            $("form").html(
+                '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
+            );
         });
+
+        event.preventDefault();
     });
 });
-// $(document).ready(function () {
-//     $("form").submit(function (event) {
-//         $(".form-group").removeClass("has-error");
-//         $(".help-block").remove();
-
-//         var formData = {
-//             name: $("#name").val(),
-//             email: $("#email").val(),
-//             subject: $("#subject").val(),
-//             message: $("#message").val(),
-//         };
-  
-//         $.ajax({
-//             type: "POST",
-//             url: "https://downnorthgarlic.com/forms/contact.php",
-//             data: formData,
-//             dataType: "json",
-//             encode: true,
-//             success: function(data) { console.log("success") },
-//             error: function(ts) { console.log("failed") }
-//         }).done(function (data) {
-//             console.log(data);
-
-//             if (!data.success) {
-//                 console.log(data);
-//                 $("form").html(
-//                     '<div class="alert alert-danger">Form inputs were wrong, please try again.</div>'
-//                 );
-//             } else {
-//                 $("form").html(
-//                     '<div class="alert alert-success">' + data.message + "</div>"
-//                 );
-//             }
-//         })
-//         .fail(function (data) {
-//             console.log("form data sent: ")
-//             console.log(formData)
-//             console.log(data);
-//             $("form").html(
-//                 '<div class="alert alert-danger">Could not reach server, please try again later.</div>'
-//             );
-//         });
-
-//         event.preventDefault();
-//     });
-// });
